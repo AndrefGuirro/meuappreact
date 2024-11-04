4394d58e8798416ff7a4b15d8496ec6dce847bf9
AndrefGuirro <131619465+AndrefGuirro@users.noreply.github.com>
2024-11-03 23:27:48 -0300

    craindo atv 03
    incompleto

---
+++ src/Atv03.js
@@ -0,0 +1,9 @@
+// Atv03.js
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+
+
+<Link to="/" className="btn btn-primary mt-5">Voltar</Link>
+
+export default Atv03;
--- src/Home.js
+++ src/Home.js
@@ -5,17 +5,16 @@ import { Link } from 'react-router-dom';
 function Home() {
   return (
     <div>
-      <h1>Página Inicial</h1>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/Atv01" className="btn btn-primary m-2">Atividade 01</Link>
-          </li>
-          <li>
+      <h1>LABORATÓRIO ENGENHARIA DE SOFTWARE</h1>
+      <h2>Atividades React</h2>    
+        
+          
+            <Link to="/Atv01" className="btn btn-primary m-2">Atividade 01</Link>          
+          
             <Link to="/Atv02" className="btn btn-primary m-2">Atividade 02</Link>
-          </li>
-        </ul>
-      </nav>
+
+            <Link to="/Atv03" className="btn btn-primary m-2">Atividade 03</Link>        
+      
     </div>
   );
 }
